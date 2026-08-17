f2171de77163320640ca42f8a5ee7f22eff64ae2
Tushar Choudhari <tusharchoudhari1710@gmail.com>
2026-08-17 10:48:50 +0530

    Update Test5Application.java

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test5Application {
 
 	public static void main(String[] args) {
+		char ch = "x";
 		SpringApplication.run(Test5Application.class, args);
 	}
 
